af8c46d299a2ffc38deb065a49c1eb28dad4529f
Liliia Novikova <kplily85@gmail.com>
2020-12-12 23:31:28 +0200

    add handleBackdropClick

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -23,12 +23,19 @@ export default class Modal extends Component {
     }
   };
 
+  handleBackdropClick = e => {
+    //    console.log('e.currentTarget: ', e.currentTarget);
+    //   console.log('e.target: ', e.target);
+
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
   render() {
     return createPortal(
-      <div className={s.Overlay}>
-        <div className={s.Modal} onClick={this.handleBackdrop}>
-          {this.props.children}
-        </div>
+      <div className={s.Overlay} onClick={this.handleBackdropClick}>
+        <div className={s.Modal}>{this.props.children}</div>
       </div>,
       modalRoot,
     );
